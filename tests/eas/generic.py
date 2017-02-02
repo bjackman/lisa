644fe4bf34713a5254e85b0dc1ba109c6b2afdb8
Brendan Jackman <brendan.jackman@arm.com>
2017-02-02 10:25:34 +0000

    generic: copyright

--- tests/eas/generic.py
+++ tests/eas/generic.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
-# Copyright (C) 2016, ARM Limited and contributors.
+# Copyright (C) 2016-2017, ARM Limited and contributors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License.
